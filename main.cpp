da67fb09731d70e4bb531813f0298cd65662c2f7
Samuel Desmond Kurtzer <samuelk373@gmail.com>
2020-02-27 22:02:30 +1030

    added middle map step

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 #include "define.h"
 #include "sdlHelper.h"
 
-//The tile
 class Tile{
 	//use the rect to know where to display.
 	//change the texture based on the value of mtype.
@@ -37,7 +36,10 @@ class Tile{
         bool getType(){
 			return tileType;
 		}
-        
+		
+		SDL_Rect* getBoundingBox(){
+			return &tileBoundingBox;
+		}	
     private:
         //The attributes of the tile
         SDL_Rect tileBoundingBox;
@@ -45,6 +47,52 @@ class Tile{
         //The tile type
         int tileType;
 };
+
+class GOL{
+	private:
+		vector<vector<bool>> map;
+	public:
+		/*
+		 * Any live cell with fewer than two live neighbours dies (referred to as underpopulation or exposure[1]).
+		 * Any live cell with more than three live neighbours dies (referred to as overpopulation or overcrowding).
+		 * Any live cell with two or three live neighbours lives, unchanged, to the next generation.
+		 * Any dead cell with exactly three live neighbours will come to life.
+		 */
+		GOL(unsigned height, unsigned width, string path){}
+		vector<vector<bool>> step(){
+			vector<vector<bool>> nextMap(map);
+			// the inner section that can be looped through without a problem.
+			for(int i=1; i<map.size()-1; i++){
+				for(int j=1; j<map[0].size()-1; j++){
+					int live = 0;
+					// The window that needs to be accounted for
+					for(int i_indent: {-1,0,1}){
+						for(int j_indent: {-1,0,1}){
+							// increment the number of live cells
+							if(!(i_indent==0 && j_indent==0) && map[i+i_indent][j+j_indent]){
+								live++;
+							}
+						}
+					}
+					// Set the cell depending on the number alive.
+					if(map[i][j]){
+						if(live < 2 || live >= 4){
+							nextMap[i][j] = false;
+						}else{
+							nextMap[i][j] = true;
+						}
+					}else if(live > 3){
+						nextMap[i][j] = true;
+					}
+				}
+			}
+			// now do the top & bottom section
+			for(int i=0; i<map.size(); i++){
+				
+			}
+		}
+};
+
 
 int main(int argc, char** argv) {
 	bool quit = false;
@@ -55,10 +103,18 @@ int main(int argc, char** argv) {
 	SDL_Window * window = SDL_CreateWindow("SDL2 Displaying Image",
 		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WWIDTH, WHIGHT, 0);
 
+	// Generate all textures
 	SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
-	SDL_Surface * image = SDL_LoadBMP("./02_getting_an_image_on_the_screen/hello_world.bmp");
-	SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, image);
+	SDL_Surface * whiteSquare = SDL_LoadBMP("./white.bmp");
+	SDL_Texture * whiteTexture = SDL_CreateTextureFromSurface(renderer, whiteSquare);
+	SDL_Surface * blackSquare = SDL_LoadBMP("./black.bmp");
+	SDL_Texture * blackTexture = SDL_CreateTextureFromSurface(renderer, blackSquare);
 
+	// generate all tiles and store in array
+		// 2d array or 1d?
+		// seperate array to track updates?
+		//
+	vector<vector<Tile>> tiles;
 	while (!quit)
 	{
 		SDL_WaitEvent(&event);
@@ -69,17 +125,21 @@ int main(int argc, char** argv) {
 				quit = true;
 				break;
 		}
+		// 
+		Tile a(10, 10, 1);
+		Tile b(21, 21, 1);
+		// Iterate over Tiles updaating if needed
+		for(auto iterVector: tiles){
+			for(auto iter: iterVector){
 
-		SDL_Rect dstrect = { 5, 5, 320, 240 };
-		SDL_Rect estrect = { 500, 500, 320, 240 };
-		SDL_RenderCopy(renderer, texture, NULL, &dstrect);
-		SDL_RenderCopy(renderer, texture, NULL, &estrect);
+		SDL_RenderCopy(renderer, texture, NULL, a.getBoundingBox());
+		SDL_RenderCopy(renderer, texture, NULL, b.getBoundingBox());
 		//SDL_RenderCopy(renderer, texture, NULL, NULL);
 		SDL_RenderPresent(renderer);
 	}
 
 	SDL_DestroyTexture(texture);
-	SDL_FreeSurface(image);
+	SDL_FreeSurface(whiteSquare);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 
